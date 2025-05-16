e0f2c7d96b32592eb38e82e59cb7e2ad76a05b7f
MEMMEDOFF021 <yusifmemmedoff021@gmail.com>
2025-05-16 15:32:39 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,41 @@
-const fs = require('fs');
-let content = '';
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+const Redis = require('ioredis');
+const cors = require('cors');
 
-// Sayt açıldıqda mövcud məlumatı oxu
-fs.readFile('notepad.txt', 'utf8', (err, data) => {
-    if (!err) content = data;
-});
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+app.use(cors());
+
+// Redis bağlantısı
+const redis = new Redis(process.env.REDIS_URL); // Render Redis URL
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Yeni qoşulan istifadəçiyə mövcud mətni göndər
-    socket.emit('loadContent', content);
+  const room = socket.handshake.headers.referer.split('/').pop();
 
-    socket.on('textChange', (newText) => {
-        content = newText;
+  socket.join(room);
 
-        // Bütün istifadəçilərə yeniləməni göndər
-        socket.broadcast.emit('textChange', newText);
+  // Redis-dən mövcud məlumatı al
+  redis.get(room, (err, content) => {
+    if (!err && content) {
+      socket.emit('code', content);
+    }
+  });
 
-        // Yaddaşa yaz
-        fs.writeFile('notepad.txt', content, (err) => {
-            if (err) console.log("Yazmaqda xəta:", err);
-        });
-    });
+  socket.on('code', ({ room, content }) => {
+    socket.to(room).emit('code', content);
+
+    // Redis-ə yadda yaz
+    redis.set(room, content);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`${PORT} portunda server işləyir`);
 });
